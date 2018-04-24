afe37c0b1df1c052f3833f114f68ffe7eb186b1a
ChastainMarchildon <200214129@student.georgianc.on.ca>
2018-04-24 14:34:42 -0400

    completed tasks 1,2,3

--- VehicleProject/Form1.Designer.cs
+++ VehicleProject/Form1.Designer.cs
@@ -29,24 +29,62 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.available = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.dataGridView1);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(260, 237);
+            this.panel1.Size = new System.Drawing.Size(610, 237);
             this.panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(610, 237);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // available
+            // 
+            this.available.Location = new System.Drawing.Point(12, 255);
+            this.available.Name = "available";
+            this.available.Size = new System.Drawing.Size(75, 23);
+            this.available.TabIndex = 1;
+            this.available.Text = "Available";
+            this.available.UseVisualStyleBackColor = true;
+            this.available.Click += new System.EventHandler(this.available_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(152, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Sold";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(634, 311);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.available);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -54,6 +92,9 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button available;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- VehicleProject/Form1.cs
+++ VehicleProject/Form1.cs
@@ -20,130 +20,100 @@ namespace VehicleProject
 
         }
 
-        private void loadTable()
+        private DataTable loadTable()
         {
-            string textToWrite = "";
-            string[] headers = { "Make", "Model", "Year", "Price", "Sold Date" };
-            TableLayoutPanel carTable = new TableLayoutPanel();
-            carTable.Location = new Point(12, 12);
-            carTable.Size = new Size(260, 200);
-            carTable.ColumnCount = 5;
-            carTable.RowCount = 4;
-            panel1.Controls.Add(carTable);
+            DataTable carData = new DataTable();
 
             try
             {
-                string connectionString = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = 'c:\users\owner\source\repos\VehicleProject\VehicleProject\Database1.mdf'; Integrated Security = True";
+                string connectionString = @"Data Source=radproject.database.windows.net;Initial Catalog=RADProject;User id=User;Password=asdFGH123;Integrated Security=True;Trusted_Connection=False;Encrypt=True;";
                 SqlConnection connection = new SqlConnection(connectionString);
                 connection.Open();
 
-                string query = "SELECT * FROM Vehicle";
+                string query = "SELECT (SELECT modelName from VehicleType WHERE VehicleType.VehicleTypeId = (SELECT VehicleTypeID FROM Model WHERE Model.ModelId = Vehicle.ModelId))AS Model, Make.makeName,modelYear,Price,SoldDate FROM Vehicle JOIN Model ON Model.ModelId = Vehicle.ModelId JOIN Make ON Make.MakeId = Vehicle.MakeId;";
+                               
                 SqlCommand command = new SqlCommand(query, connection);
 
                 SqlDataReader dr = command.ExecuteReader();
-
-                while (dr.HasRows)
-                {
-                    ArrayList data = new ArrayList();
-
-                    string make = "";
-                    int Make = dr.GetOrdinal("MakeId");
-                    make = getMake(Make);
-                    data.Add(make);
-
-                    string model = "";
-                    int Model = dr.GetOrdinal("ModelId");
-                    model = getModel(Model);
-                    data.Add(model);
-
-                    string year = "";
-                    if (dr.Read())
-                        year = String.Format("%d", dr.GetInt32(dr.GetOrdinal("Year")));
-                    data.Add(year);
-
-                    string price = "";
-                    if (dr.Read())
-                        price = String.Format("{0:0.##}", dr.GetDecimal(dr.GetOrdinal("Price")));
-                    data.Add(price);
-
-                    string sold = "";
-                    if (dr.Read())
-                        sold = String.Format("%d", dr.GetDateTime(dr.GetOrdinal("SoldDate")));
-                    data.Add(sold);
-
-                    carTable.RowStyles.Add(new RowStyle(SizeType.Absolute, 20f));
-
-                    for (int colCount = 0; colCount < 5; colCount++)
-                    {
-                        for (int i = 0; i < data.Count; i++)
-                        {
-                            Label label = new Label();
-                            if (i == 0)
-                            {
-                                textToWrite = headers[colCount];
-                            }
-                            else
-                            {
-                                textToWrite = String.Format("%d", data[i]);
-                            }
-                            label.Text = textToWrite;
-                            carTable.Controls.Add(label, colCount, i);
-                        }
-                    }
-                }
+                carData.Load(dr);
 
                 connection.Close();
             }
+
             catch (Exception e)
             {
                 Console.Write(e.GetBaseException());
             }
-        }
-    
-
-        private string getMake(int MakeId)
-        {
-            string makeName = "";
-            string connectionString = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = 'c:\users\owner\source\repos\VehicleProject\VehicleProject\Database1.mdf'; Integrated Security = True";
-            SqlConnection connection = new SqlConnection(connectionString);
-            connection.Open();
-
-            string query = "SELECT Name FROM Make WHERE MakeId = "+MakeId;
-            SqlCommand command = new SqlCommand(query, connection);
-
-            SqlDataReader dr = command.ExecuteReader();
-            if(dr.Read())
-            {
-                makeName = dr.GetString(dr.GetOrdinal("Name"));
-            }
-            connection.Close();
-            return makeName;
-        
-        }
-        private string getModel(int ModelId)
-        {
-            string modelName = "";
-            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=c:\users\owner\source\repos\VehicleProject\VehicleProject\Database1.mdf;Integrated Security=True";
-            SqlConnection connection = new SqlConnection(connectionString);
-            connection.Open();
-
-            string query = "SELECT Name FROM VehicleType WHERE VehicleTypeId = (SELECT VehicleTypeId FROM Model WHERE ModelId = " + ModelId +")";
-            SqlCommand command = new SqlCommand(query, connection);
-
-            SqlDataReader dr = command.ExecuteReader();
-            if (dr.Read())
-            {
-                modelName = dr.GetString(1);
-            }
-            connection.Close();
-            return modelName;
+            return carData;
         }
 
         private void Form1_Load_1(object sender, EventArgs e)
         {
             panel1.Visible = true;
-            loadTable();
+            dataGridView1.DataSource = loadTable();
+        }
 
+        private void available_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = showAvailable();
+        }
+
+        private DataTable showAvailable()
+        {
+            DataTable carData = new DataTable();
+
+            try
+            {
+                string connectionString = @"Data Source=radproject.database.windows.net;Initial Catalog=RADProject;User id=User;Password=asdFGH123;Integrated Security=True;Trusted_Connection=False;Encrypt=True;";
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                string query = "SELECT (SELECT modelName from VehicleType WHERE VehicleType.VehicleTypeId = (SELECT VehicleTypeID FROM Model WHERE Model.ModelId = Vehicle.ModelId))AS Model, Make.makeName,modelYear,Price,SoldDate FROM Vehicle JOIN Model ON Model.ModelId = Vehicle.ModelId JOIN Make ON Make.MakeId = Vehicle.MakeId WHERE SoldDate IS NULL;";
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                SqlDataReader dr = command.ExecuteReader();
+                carData.Load(dr);
+
+                connection.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.Write(e.GetBaseException());
+            }
+            return carData;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = showSold();
+        }
+        private DataTable showSold()
+        {
+            DataTable carData = new DataTable();
+
+            try
+            {
+                string connectionString = @"Data Source=radproject.database.windows.net;Initial Catalog=RADProject;User id=User;Password=asdFGH123;Integrated Security=True;Trusted_Connection=False;Encrypt=True;";
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                string query = "SELECT (SELECT modelName from VehicleType WHERE VehicleType.VehicleTypeId = (SELECT VehicleTypeID FROM Model WHERE Model.ModelId = Vehicle.ModelId))AS Model, Make.makeName,modelYear,Price,SoldDate FROM Vehicle JOIN Model ON Model.ModelId = Vehicle.ModelId JOIN Make ON Make.MakeId = Vehicle.MakeId WHERE SoldDate IS NOT NULL;";
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                SqlDataReader dr = command.ExecuteReader();
+                carData.Load(dr);
+
+                connection.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.Write(e.GetBaseException());
+            }
+            return carData;
         }
     }
 }
